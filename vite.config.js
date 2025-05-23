14216a668688fd8c4bec5273e6fdbada4a5f30e9
roqcodes <roq2362005@gmail.com>
2025-05-24 04:09:01 +0530

    E

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+  },
   server: {
     port: 3000,
     proxy: {
